f561fb9a4004dfe8bf705549b02cb8a286ec3583
Cristian Toaquiza <crissmtl@gmail.com>
2016-01-29 09:17:46 -0500

    Initial commit: Crear esqueleto del Cliente

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = angular.module('paintboard', ['ui.router', 'ngAnimate', 'ngResource', 'toastr']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('chat', {
+      url: "/chat",
+      templateUrl: "rutas/chat.html",
+      //controller: 'HomeController'
+    });
+  $urlRouterProvider.otherwise("/chat");
+});
