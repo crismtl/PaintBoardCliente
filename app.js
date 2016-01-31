2ee4e3e953ab56f20587d23435b2e43098bf5f55
jennygabriela47 <jennymq44@gmail.com>
2016-01-31 01:46:13 -0500

    Añadi los htmls que faltaban

--- app.js
+++ app.js
@@ -7,10 +7,30 @@ app.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: "rutas/chat.html",
       controller: 'ChatController'
     })
+    .state('home', {
+      url: "/home",
+      templateUrl: "rutas/home.html",
+    //  controller: 'ChatController'
+    })
+    .state('acerca', {
+      url: "/acerca",
+      templateUrl: "rutas/acerca.html",
+    //  controller: 'ChatController'
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "rutas/login.html",
+    //  controller: 'ChatController'
+    })
+    .state('registro', {
+      url: "/registro",
+      templateUrl: "rutas/registro.html",
+    //  controller: 'ChatController'
+    })
     .state('canvas', {
       url: "/canvas",
       templateUrl: "rutas/canvas.html",
       controller: 'CanvasController'
     });
-  $urlRouterProvider.otherwise("/chat");
+  $urlRouterProvider.otherwise("/home");
 });
