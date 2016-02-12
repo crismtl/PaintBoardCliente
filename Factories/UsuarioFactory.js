49732c5d71c390cd0829b93373b81b718cf3b3f6
Cristian Toaquiza <crissmtl@gmail.com>
2016-02-12 16:56:08 -0500

    Crear función login en UsuarioFactory

--- Factories/UsuarioFactory.js
+++ Factories/UsuarioFactory.js
@@ -2,7 +2,14 @@ app.factory('UsuarioFactory', ['$resource', function($resource) {
   var factory = $resource('http://localhost:1337/Usuario/:idUsuario', {
     idUsuario: '@idUsuario'
   }, {
-    update: {
+    actualizar: {
+      method: 'PUT',
+      params: {
+        idUsuario: '@idUsuario'
+      }
+    }
+  }, {
+    login: {
       method: 'PUT',
       params: {
         idUsuario: '@idUsuario'
