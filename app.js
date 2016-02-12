0d5c07d4a5b79938cc5386ba85dd1a0926f3f731
Cristian Toaquiza <crissmtl@gmail.com>
2016-02-12 12:35:11 -0500

    Configurar login y rutas

--- app.js
+++ app.js
@@ -1,51 +1,77 @@
 io.sails.url = "http://localhost:1337";
 
-var app = angular.module('paintboard', ['ui.router', 'ngAnimate', 'ngResource', 'toastr', 'ngFacebook']);
+var app = angular.module('paintboard', ['ui.router', 'ngAnimate', 'ngResource', 'toastr', 'ngFacebook', 'ngCookies']);
 
 app.config(function($stateProvider, $urlRouterProvider, $facebookProvider) {
-    console.log('entro config');
     $facebookProvider.setAppId('946903475391684');
     $stateProvider
-        .state('chat', {
-            url: "/chat",
-            templateUrl: "rutas/chat.html",
-            controller: 'ChatController'
-        })
         .state('home', {
             url: "/",
             templateUrl: "rutas/home.html",
-            //  controller: 'ChatController'
+            //  controller: 'ChatController',
+            data: {
+                nivelDeAcceso: 0,
+                loginRequerido: false
+            }
         })
         .state('acercade', {
             url: "/acercade",
             templateUrl: "rutas/acercade.html",
-            //  controller: 'ChatController'
-        })
-        .state('login', {
-            url: "/login",
-            templateUrl: "rutas/login.html",
-            controller: 'LoginController'
+            //  controller: 'ChatController',
+            data: {
+                nivelDeAcceso: 0,
+                loginRequerido: false
+            }
         })
         .state('registro', {
             url: "/registro",
             templateUrl: "rutas/registro.html",
-            controller: 'RegistroController'
+            controller: 'RegistroController',
+            data: {
+                nivelDeAcceso: 0,
+                loginRequerido: false
+            }
         })
-        .state('canvas', {
-            url: "/canvas",
-            templateUrl: "rutas/canvas.html",
-            controller: 'CanvasController'
+        .state('login', {
+            url: "/login",
+            templateUrl: "rutas/login.html",
+            controller: 'LoginController',
+            data: {
+                nivelDeAcceso: 0,
+                loginRequerido: false
+            }
         })
         .state('paintboard', {
             url: "/paintboard",
             templateUrl: "rutas/paintboard.html",
-            //controller: 'CanvasController'
+            //controller: 'CanvasController',
+            data: {
+                nivelDeAcceso: 1,
+                loginRequerido: true
+            }
+        })
+        .state('canvas', {
+            url: "/canvas",
+            templateUrl: "rutas/canvas.html",
+            controller: 'CanvasController',
+            data: {
+                nivelDeAcceso: 1,
+                loginRequerido: true
+            }
+        })
+        .state('chat', {
+            url: "/chat",
+            templateUrl: "rutas/chat.html",
+            controller: 'ChatController',
+            data: {
+                nivelDeAcceso: 1,
+                loginRequerido: true
+            }
         });
     $urlRouterProvider.otherwise("/");
 });
 
-app.run(function($rootScope) {
-    console.log('entro run');
+app.run(function($rootScope, $cookies, $state) {
     // Load the facebook SDK asynchronously
     (function() {
         // If we've already installed the SDK, we're done
@@ -66,4 +92,23 @@ app.run(function($rootScope) {
         // Insert the Facebook JS SDK into the DOM
         firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
     }());
+
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams) {
+        var requiereLogin = toState.data.loginRequerido;
+        var nivelAcceso = toState.data.nivelDeAcceso;
+
+        if (requiereLogin) {
+            console.log('Si require Login');
+            if ($cookies.get('UsuarioId')) {
+                console.log('hizo Login');
+                console.log(nivelAcceso);
+            } else {
+                console.log('No ha hecho Login');
+                event.preventDefault();
+                return $state.go('login')
+            }
+        } else {
+            console.log('No requiere login');
+        }
+    });
 });
