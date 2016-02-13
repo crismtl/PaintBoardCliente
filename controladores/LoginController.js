7f91c29bf7bd2aa8dda5e33170294871f6de1980
Cristian Toaquiza <crissmtl@gmail.com>
2016-02-12 23:48:54 -0500

    Añadir cookies a LoginController

--- controladores/LoginController.js
+++ controladores/LoginController.js
@@ -1,40 +1,46 @@
-app.controller('LoginController', ['$scope', 'UsuarioFactory', '$facebook', function($scope, UsuarioFactory, $facebook) {
-    $scope.isLoggedIn = false;
-    $scope.login = function() {
-        $facebook.login().then(function() {
-            refresh();
-        });
-    }
-
-    function refresh() {
-        $facebook.api("/me").then(
-            function(response) {
-                console.log(JSON.stringify(response));
-                $scope.welcomeMsg = "Welcome " + response.name;
-                $scope.isLoggedIn = true;
-            },
-            function(err) {
-                $scope.welcomeMsg = "Please log in";
+app.controller('LoginController', ['$scope', 'UsuarioFactory', '$state', '$cookies', '$facebook',
+    function($scope, UsuarioFactory, $state, $cookies, $facebook) {
+        $scope.isLoggedIn = false;
+        $scope.login = function() {
+            $facebook.login().then(function() {
+                refresh();
             });
-    }
-    refresh();
+        }
 
-    $scope.usuario = {
-        email: '',
-        password: ''
-    }
+        function refresh() {
+            $facebook.api("/me").then(
+                function(response) {
+                    console.log(JSON.stringify(response));
+                    $scope.welcomeMsg = "Welcome " + response.name;
+                    $scope.isLoggedIn = true;
+                },
+                function(err) {
+                    $scope.welcomeMsg = "Please log in";
+                });
+        }
+        refresh();
 
-    $scope.loginConModelo = function() {
-        UsuarioFactory.login({
-            email: $scope.usuario.email,
-            password: $scope.usuario.password
-        }).$promise.then(
-            function success(respuesta) {
-                console.log('Logueado con modelo');
-                console.log(respuesta);
-            },
-            function error(error) {
-                console.log(error);
-            });
+        $scope.usuario = {
+            email: '',
+            password: ''
+        }
+
+        $scope.loginConModelo = function() {
+            UsuarioFactory.login({
+                email: $scope.usuario.email,
+                password: $scope.usuario.password
+            }).$promise.then(
+                function success(respuesta) {
+                    console.log(respuesta.id);
+                    console.log(respuesta.email);
+                    $cookies.put('UsuarioId', respuesta.id);
+                    console.log('Logueado con modelo');
+                    console.log(respuesta);
+                    $state.go('paintboard');
+                },
+                function error(error) {
+                    console.log(error);
+                });
+        }
     }
-}]);
+]);
